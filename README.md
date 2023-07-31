a249960d8806e22575061e888df0709063b27ba4
Jairo Morales Pérez <jairomoralesperez0@gmail.com>
2023-07-31 20:13:27 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # startpage
+My Personal Startpage
+
+![Screenshot](/docs/images/startpage-1.png)
+
+![Screenshot](/docs/images/startpage-2.png) 
+
+## Acknowledgements
+
+- **Thanks to**
+
+  - [Ameer-Moustafa](https://github.com/Ameer-Moustafa/Forbidden-startpage)
---
+++ script/themes.js
@@ -0,0 +1,23 @@
+themeRandomizer = () => {
+const theme = ["catppuccin"]
+const random = theme[Math.floor(Math.random() * theme.length)]
+return random
+}
+
+setTheme = () => {
+    const selector = document.getElementsByClassName("image")[0]
+    savedTheme = localStorage.getItem('theme')
+    if(savedTheme) {
+        document.documentElement.className = savedTheme
+    }
+
+
+
+    selector.addEventListener("click", () => {
+        theme = themeRandomizer()
+        document.documentElement.className = theme
+        localStorage.setItem('theme', theme)
+    })
+}
+
+setTheme()
